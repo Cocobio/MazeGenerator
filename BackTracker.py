84b974ff76a46db0686527934ceb0f1c8e1c6fce
Cocobio <iosorio@udec.cl>
2020-06-30 04:42:45 -0400

    MazeGenerator BackTracker algorithm encapsulated

---
+++ BackTracker.py
@@ -0,0 +1,86 @@
+import pygame
+import numpy as np
+from random import randint
+
+class BackTracker:
+	# Creates rectangle for maze potition
+	box_boarder_color = (24, 27, 30)
+	box_fill_color = (78, 245, 236)
+	box_visited_fill_color = (255, 174, 43)
+	box_current_fill_color = (0, 255, 0)
+	box_stack_color = (18, 94, 161)
+
+	def __init__(self, maze, box):
+		self.stack = []
+		self.maze = maze
+		self.box = box
+		self.visited = np.zeros(self.maze.shape)
+
+		self.generated = False
+
+	def iterate(self):
+		if len(self.stack):
+			self.current_node = self.stack.pop()
+			neighbor_not_checked = len(self.current_node.neighbor)
+			if neighbor_not_checked != 0:
+				self.stack.append(self.current_node)
+				r = self.current_node.neighbor.pop(randint(0, neighbor_not_checked-1))
+
+				if self.visited[r] == 0:
+					self.visited[r] = 1
+					self.current_node.remove_wall(self.maze[r])
+					self.current_node = self.maze[r]
+
+					self.stack.append(self.current_node)
+			return self.current_node
+		elif self.visited[0,0] == 0:
+			self.visited[0,0] = 1
+			self.stack.append(self.maze[0,0])
+			self.current_node = self.maze[0,0]
+		else:
+			self.generated = False
+
+			return self.maze[0,0]
+
+	def draw(self, screen):
+		# Update maze
+		# current_node = backtracker(stack, self.maze, self.visited)
+
+		# Drawing maze
+		for i in range(self.maze.shape[0]):
+			for j in range(self.maze.shape[1]):
+				if self.visited[i,j] == 0:
+					color = self.box_fill_color
+				else:
+					color = self.box_visited_fill_color
+				self.draw_node(self.maze[i,j], screen, self.box_boarder_color, color)
+
+		# Drawing nodes that are still on the stack
+		for n in self.stack:
+			self.draw_node(n, screen, self.box_boarder_color, self.box_stack_color)
+		# Drawing current Position
+		self.draw_node(self.current_node, screen, self.box_boarder_color, self.box_current_fill_color)
+
+	def draw_node(self, node, screen, boarder_color, fill_color):
+		x, y = node.position
+		current_box = self.box.move((self.box.width*x,self.box.height*y))
+
+		thicc = 3
+
+		pygame.draw.rect(screen, fill_color, current_box)
+		if node.north_boarder:
+			pygame.draw.line(screen, boarder_color, (self.box.width*x, self.box.height*y), 		(self.box.width*(x+1), self.box.height*y),	thicc)
+		if node.south_boarder:
+			pygame.draw.line(screen, boarder_color, (self.box.width*x, self.box.height*(y+1)), 	(self.box.width*(x+1), self.box.height*(y+1)),thicc)
+		if node.east_boarder:
+			pygame.draw.line(screen, boarder_color, (self.box.width*x, self.box.height*y),		(self.box.width*x, self.box.height*(y+1)),	thicc)
+		if node.west_boarder:
+			pygame.draw.line(screen, boarder_color, (self.box.width*(x+1), self.box.height*y),	(self.box.width*(x+1), self.box.height*(y+1)),thicc)
+
+
+
+
+
+
+
+
--- WindowController.py
+++ WindowController.py
@@ -11,43 +11,9 @@
 
 import pygame
 import numpy as np
-from random import randint
-
-##########################################################################################################################
-def draw_node(node, screen, box, box_boarder_color, box_fill_color):
-	x, y = node.position
-	current_box = box.move((box.width*x,box.height*y))
-
-	thicc = 3
-
-	pygame.draw.rect(screen, box_fill_color, current_box)
-	if node.north_boarder:
-		pygame.draw.line(screen, box_boarder_color, (box.width*x, box.height*y), 		(box.width*(x+1), box.height*y),	thicc)
-	if node.south_boarder:
-		pygame.draw.line(screen, box_boarder_color, (box.width*x, box.height*(y+1)), 	(box.width*(x+1), box.height*(y+1)),thicc)
-	if node.east_boarder:
-		pygame.draw.line(screen, box_boarder_color, (box.width*x, box.height*y),		(box.width*x, box.height*(y+1)),	thicc)
-	if node.west_boarder:
-		pygame.draw.line(screen, box_boarder_color, (box.width*(x+1), box.height*y),	(box.width*(x+1), box.height*(y+1)),thicc)
-
-def backtracker(stack, maze, visited):
-	if len(stack):
-		current_node = stack.pop()
-		neighbor_not_checked = len(current_node.neighbor)
-		if neighbor_not_checked != 0:
-			stack.append(current_node)
-			r = current_node.neighbor.pop(randint(0, neighbor_not_checked-1))
-
-			if visited[r] == 0:
-				visited[r] = 1
-				current_node.remove_wall(maze[r])
-				current_node = maze[r]
-
-				stack.append(current_node)
-		return current_node
-	else:
-		return maze[0][0]
-##########################################################################################################################
+from BackTracker import BackTracker
+import datetime
+import os
 
 # Screen size
 width = 800
@@ -56,13 +22,6 @@ height = 800
 # maze parameters
 maze_rows = 42
 maze_columns = 42
-# Creates rectangle for maze potition
-box = pygame.Rect(0, 0, width/maze_columns, height/maze_rows)
-box_boarder_color = (24, 27, 30)
-box_fill_color = (78, 245, 236)
-box_visited_fill_color = (255, 174, 43)
-box_current_fill_color = (0, 255, 0)
-box_stack_color = (18, 94, 161)
 
 pygame.init()
 
@@ -73,8 +32,7 @@ icon = pygame.image.load("icon.png")
 pygame.display.set_icon(icon)
 
 # Maze spects
-class node:
-
+class node: 
 	def __init__(self, position):
 		self.position = position
 
@@ -102,8 +60,8 @@ class node:
 			self.east_boarder = False
 			other_node.west_boarder = False
 
-maze = np.ones((maze_columns, maze_rows), dtype=node)
-visited = np.zeros((maze_columns, maze_rows))
+maze = np.empty((maze_columns, maze_rows), dtype=node)
+box = pygame.Rect(0, 0, width/maze_columns, height/maze_rows)
 
 for i in range(maze_columns):
 	for j in range(maze_rows):
@@ -118,40 +76,39 @@ for i in range(maze_columns):
 			maze[i][j].neighbor.append((i,   j+1))
 
 # depth first algorithm
-stack = []
-visited[0][0] = 1
-stack.append(maze[0][0])
-
+algorithm = BackTracker(maze, box)
+print(datetime.datetime.now())
 running = True
 
 while running:
+	# timer
 	pygame.time.wait(2)
+	
+	# event attender
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_PRINT:
-				pygame.image.save(screen, "screenshot.jpeg")
+				if not os.path.isdir('./screenshots'):
+					os.mkdir('screenshots') 
 
+				name = 'screenshots/screenshot'
+				if algorithm.generated:
+					name += '_generated_'
+				else:
+					name += '_generating_'
+				name += str(datetime.datetime.now()).replace(':', '-')
+				name += ".jpeg"
+				pygame.image.save(screen, name)
+
+	# Next iteration
+	algorithm.iterate()
+
+	# OpenGL Stuff
 	screen.fill((255,255,255))
 
-	# Update maze
-	current_node = backtracker(stack, maze, visited)
-
-	# Drawing maze
-	for i in range(maze_columns):
-		for j in range(maze_rows):
-			if visited[i][j] == 0:
-				color = box_fill_color
-			else:
-				color = box_visited_fill_color
-			draw_node(maze[i][j], screen, box, box_boarder_color, color)
-
-	# Drawing nodes that are still on the stack
-	for n in stack:
-		draw_node(n, screen, box, box_boarder_color, box_stack_color)
-	# Drawing current Position
-	draw_node(current_node, screen, box, box_boarder_color, box_current_fill_color)
+	algorithm.draw(screen)
 
 	pygame.display.update()
 
